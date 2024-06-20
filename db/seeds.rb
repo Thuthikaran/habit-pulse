97b33af23d8c0fd43b0244383111537b7881ecb8
Pedro Leal <lealpedro2009@gmail.com>
2024-06-20 18:25:50 +0100

    seeds users

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# create 3 users with faker gem Thuthikaran Easvaran
+User::create!(email: "pedro@mail.com", password: 'password', password_confirmation: 'password', first_name: 'Pedro', last_name: 'Leal')
+User::create!(email: "thuthi@mail.com", password: 'password', password_confirmation: 'password', first_name: 'Thuthikaran', last_name: 'Easvaran')
+User::create!(email: "sahba@mail.com", password: 'password', password_confirmation: 'password', first_name: 'Sahba', last_name: 'Azhari')
+User::create!(email: "sahba@mail.com", password: 'password', password_confirmation: 'password', first_name: 'Jeannine', last_name: 'Vernon')
