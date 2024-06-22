8550e655b08123c3bf881d1453618931b977761f
Pedro Leal <lealpedro2009@gmail.com>
2024-06-22 16:48:22 +0100

    auto generate occurrences based on habit start to end date, and habit frequency, when habit occurrence completion_status is updated to complete or missed, it updates the habit_statics table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_19_183806) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_22_130709) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,7 +41,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_19_183806) do
   end
 
   create_table "occurrences", force: :cascade do |t|
-    t.string "completion_status"
+    t.string "completion_status", default: "pending"
     t.date "date"
     t.bigint "habit_id", null: false
     t.datetime "created_at", null: false
