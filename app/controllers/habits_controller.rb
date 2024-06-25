84f9b8037a75f66724e438e7371865ba6e6d1d84
Pedro Leal <lealpedro2009@gmail.com>
2024-06-25 19:07:12 +0100

    basic create habit func

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -4,7 +4,24 @@ class HabitsController < ApplicationController
     @habits = Habit.all
   end
 
-  def new 
-    @habits = Habit.new
+  def new
+    @habit = Habit.new
+  end
+
+  def create
+
+    @habit = Habit.new(habit_params)
+    @habit.user = current_user
+    if @habit.save
+      redirect_to habits_path, notice: 'Habit was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def habit_params
+    params.require(:habit).permit(:name, :priority, :start_date, :end_date, :reminder, :frequency, :status, :category, days_of_week: [])
   end
 end
