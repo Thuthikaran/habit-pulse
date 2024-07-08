6ff59d6fff46caa47538cb58b5f4bcbbb027c7e8
Mark Carson <markcarson121@gmail.com>
2024-07-08 18:21:40 -0300

    added progress ring to show page and dashboard

---
+++ app/javascript/progressRing.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const percentageValue = document.getElementById("percentageValue");
+  const percentage = percentageValue.getAttribute("data-percentage");
+  const percentageRing = document.getElementById("percentageRing");
+  percentageRing.style.setProperty("--percentage", `${percentage}%`);
+});
