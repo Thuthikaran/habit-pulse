1897cf4aabc90fa7eac70e0d72356666cbc1890b
Thuthikaran Easvaran <thuthikaraneasvaran@gmail.com>
2024-07-09 12:08:03 +0200

    removed the frequency and reminder validation from models

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -9,10 +9,10 @@ class Habit < ApplicationRecord
   # restrict priority to 1-3
   validates :priority, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 3 }
   validates :start_date, presence: true
-  validates :frequency, inclusion: { in: FREQUENCIES }
+  # validates :frequency, inclusion: { in: FREQUENCIES }
   validates :status, inclusion: { in: STATUSES }
   validates :category, inclusion: { in: %w[Health Creativity Learning Mindfulness] }
-  validates :days_of_week, inclusion: { in: %w[Monday Tuesday Wednesday Thursday Friday Saturday Sunday] }
+  # validates :days_of_week, inclusion: { in: %w[Monday Tuesday Wednesday Thursday Friday Saturday Sunday] }
 
 
   has_many :occurrences, dependent: :destroy
