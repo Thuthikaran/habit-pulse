7dc40afea36ede4f2493fd127c9fb427c14d79de
Jess Silva Carvalho <jc.carvalho@outlook.com>
2024-07-10 13:21:43 +0100

    Handling of days_of_week input toggling in habit create form

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,7 @@
 // Import and register all your controllers from the importmap under controllers/*
 
 import { application } from "controllers/application"
+// import { definitionsFromContext } from "/stimulus/webpack-helpers"
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
@@ -11,3 +12,6 @@ eagerLoadControllersFrom("controllers", application)
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
+
+// const context = require.context("controllers", true, /_controller\.js$/)
+// application.load(definitionsFromContext(context))
