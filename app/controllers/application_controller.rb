4054aff89bd77b1c7f285d36e89e3e56b3961006
Sahba Azhari Sahba_azhari@yahoo.com <sahba_azhari@yahoo.com>
2024-07-12 17:45:03 +0200

    Add custom error pages and flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,21 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :photo])
   end
+
+  def not_found
+    render status: :not_found, template: 'errors/404'
+  end
+
+  def bad_request
+    render status: :bad_request, template: 'errors/400'
+  end
+
+  def internal_server_error
+    render status: :internal_server_error, template: 'errors/500'
+  end
+
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from ActionController::RoutingError, with: :not_found
+  rescue_from ActionController::BadRequest, with: :bad_request
+  rescue_from StandardError, with: :internal_server_error
 end
